1a49089c516a472371396977ad7a8016ae53a7de
Miss-Jaqui <107520551+Miss-Jaqui@users.noreply.github.com>
2022-08-28 23:35:55 -0500

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,30 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAL0OMi1CMKpqORiJozMNETInEEmFYZxOs",
+    authDomain: "project-c94-d9c5d.firebaseapp.com",
+    databaseURL: "https://project-c94-d9c5d-default-rtdb.firebaseio.com",
+    projectId: "project-c94-d9c5d",
+    storageBucket: "project-c94-d9c5d.appspot.com",
+    messagingSenderId: "563152523934",
+    appId: "1:563152523934:web:e073e8b819687b123ae18f"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  user_name = localStorage.getItem("user_name");
+  room_name = localStorage.getItem("room_name");
+
+
+//document.getElementById("user_name").innerHTML = "¡Hola " + user_name + "!" + "Bienvenido a la sala: " + room_name;
+
+function send() {
+    msg = document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({
+        name: user_name,
+        message: msg,
+        like: 0
+    });
+
+    document.getElementById("msg").value = "";
+}
